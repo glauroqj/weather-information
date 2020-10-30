27a789ff159ef5beeb1edcbef6c06ac5b78782cd
glauroqj <glauro.juliani@hotmail.com>
2020-10-30 18:34:10 -0300

    add: bunch of new things

--- src/assets/style/globalStyle.js
+++ src/assets/style/globalStyle.js
@@ -2,9 +2,11 @@ import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset'
 
 const GlobalStyle = createGlobalStyle`
-  ${reset}
+  ${reset};
 
   html, body {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
     webkit-font-smoothing: antialiased;
   }
 `
